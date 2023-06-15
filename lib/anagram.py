c96c5fe92fbcd0f6b05ed31dad2953b64d56f0ef
matthew rendely <matthew.rendely@gmail.com>
2023-06-14 18:51:05 -0700

    Done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, words):
+        matching_words = [word for word in words if sorted(word) == sorted(self.word)]
+        return matching_words
